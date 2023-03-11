ca26e675189b99f3dc65bdf33eddec43d88d3708
Fadi Aboona <fadi.aboona23@gmail.com>
2023-03-11 11:09:40 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "fadi",
+    streetAddress: "323" 
+}
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return{
+        ...employee,
+        [key]: value,
+    };
+}
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, value) {
+    employee[name] = value;
+    return employee
+}
+
+
+
+function deleteFromEmployeeByKey(employee, name, value) {
+    const obj1 = {
+        ...employee
+    }
+    delete obj1[name]
+    return obj1 
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, name, value) {
+    delete employee[name]
+    return employee
+    
+}
